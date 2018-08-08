3cede392592a31871b6f2ab5d487db21155d0d96
masa-laughndie <37497371+masa-laughndie@users.noreply.github.com>
2018-08-08 13:55:22 +0900

    Add counter reducer

--- src/index.js
+++ src/index.js
@@ -1,4 +1,20 @@
-import expect from 'expect';
-import './index.css';
+import expect from "expect";
+import "./index.css";
 
-console.log('all test passed!');
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return state + 1;
+    case "DECREMENT":
+      return state - 1;
+    default:
+      return state;
+  }
+};
+
+expect(counter(0, { type: "INCREMENT" })).toEqual(1);
+expect(counter(1, { type: "INCREMENT" })).toEqual(2);
+expect(counter(2, { type: "DECREMENT" })).toEqual(1);
+expect(counter(1, { type: "SOMETHING" })).toEqual(1);
+
+console.log("all test passed!");
