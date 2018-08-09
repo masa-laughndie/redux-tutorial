0677ed2d2505bba04cddb0c5149fec1bef97750b
masa-laughndie <37497371+masa-laughndie@users.noreply.github.com>
2018-08-09 17:09:38 +0900

    Add lifecycle method on the FilterLink

--- src/components/FilterLink.js
+++ src/components/FilterLink.js
@@ -3,6 +3,14 @@ import Link from "./Link";
 import store from "../store";
 
 export default class FilterLink extends Component {
+  componentDidMount() {
+    this.unsubscribe = store.subscribe(() => this.forceUpdate());
+  }
+
+  componentWillUnmount() {
+    this.unsubscribe();
+  }
+
   render() {
     const { filter, children } = this.props;
     const { visibilityFilter } = store.getState();
