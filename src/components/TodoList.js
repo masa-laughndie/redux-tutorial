58acee6a3e64934585c377364dbd41af3faf5f5e
masa-laughndie <37497371+masa-laughndie@users.noreply.github.com>
2018-08-09 13:33:25 +0900

    Fix TodoList.js

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -4,14 +4,14 @@ import Todo from "./Todo";
 export default function TodoList({ todos, onTodoClick }) {
   return (
     <ul>
-      {todos.map(todo => {
+      {todos.map(todo => (
         <Todo
           key={todo.id}
           onClick={() => onTodoClick(todo.id)}
           completed={todo.completed}
           text={todo.text}
-        />;
-      })}
+        />
+      ))}
     </ul>
   );
 }
